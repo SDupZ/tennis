2c554d55f8a7e0d3b2e2540be53180e585ceb4ea
Simon du Preez <smdupnz@yahoo.co.nz>
2016-10-09 20:23:41 +1300

    Added hero slider

---
+++ static/js/src/handleSliders.js
@@ -0,0 +1,23 @@
+/*
+     *	Hero slider
+     */
+    (function ($) {
+
+        var $heroSlider = $("#heroSlider");
+
+        $heroSlider.slick({
+            dots: false,
+            infinite: true,
+            speed: 600,
+            slidesToShow: 1,
+            arrows: true,
+            fade: true,
+            touchMove: true,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            lazyLoad: 'ondemand',
+        });
+
+
+    })(jQuery);
