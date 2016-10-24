c9ba17e2840a28d580cbea30d3d58246c75a801e
Simon du Preez <smdupnz@yahoo.co.nz>
2016-10-24 23:29:33 +1300

    Updated menu

--- static/js/src/handleMenu.js
+++ static/js/src/handleMenu.js
@@ -2,18 +2,22 @@
     var active = false;
     var menuIcon = $('.menu-icon');
     var overlay = $('.overlay-content');
+    var body = $('body');
 
     var toggleMenu = function() {
       if (!active) {
           //Show Menu
           menuIcon.addClass("is-active");
+          menuIcon.addClass("menu-icon--is-active");
           overlay.addClass("overlay-content--is-active");
+          body.addClass("noscroll");
           active = true;
       } else {
           //Hide Menu
           menuIcon.removeClass("is-active");
           overlay.removeClass("overlay-content--is-active");
-          console.log("Hide Menu");
+          menuIcon.removeClass("menu-icon--is-active");
+          body.removeClass("noscroll");
           active = false;
       }
     }
